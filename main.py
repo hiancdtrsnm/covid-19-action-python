4c217f7e097b2ea4341df9d23bb6573b3f23b925
hian <hiancdtrsnm@gmail.com>
2020-03-31 01:39:34 -0400

    Fix typos

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import json
 
 def main():
 
-    os.makedirs('data/mini')
+    os.makedirs('data/mini',exist_ok=True)
     data = json.load(open('data/covid19-cuba.json'))
-    json.dump(data, open('data/mini/covide-cuba.json', 'w'))
+    json.dump(data, open('data/mini/covid19-cuba.json', 'w'))
 
 if __name__ == "__main__":
     main()
