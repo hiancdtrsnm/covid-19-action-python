dd22878961cfebde4488dcf9556601b79131ddb9
hian <hiancdtrsnm@gmail.com>
2020-03-31 01:24:38 -0400

    Add mimification

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import os
 import requests  # noqa We are just importing this to prove the dependency installed correctly
-
+import json
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
 
-    my_output = f"Hello {my_input}"
-    print(os.listdir())
-
-    open('map.html', 'w').write('esto es una tiza de mapa')
-
-    print(f"::set-output name=myOutput::{my_output}")
-
+    os.makedirs('data/mini')
+    data = json.load(open('data/covid19-cuba.json'))
+    json.dump(data, open('data/mini/covide-cuba.json' 'w'))
 
 if __name__ == "__main__":
     main()
